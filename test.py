151f73fe297308a25c3f55e2c0af21a954611f68
ini742 <knandwan@andrew.cmu.edu>
2022-10-21 23:25:13 -0700

    testcasescomplete

--- test.py
+++ test.py
@@ -1,83 +1,223 @@
 import requests
 from warREST import play, Card
 
-BASE = "http://127.0.0.1:5000/"
+BASE = "http://127.0.0.1:5000"
 
 card_values_lower = list(range(2,8))
 card_values_higher = list(range(9,15))
 card_suits = ["spades", "hearts", "diamonds", 'clubs']
 
-def test1():
-    cl, ch = list(), list()
+# Test 1: Code functionality and logic test
+## Hardcoding deck1 with lower value cards and deck2 with higher value cards and testing play functionality
+def testPlayer2Winner():
+    lowerset, higherset = list(), list()
 
- 
+    # Hardcoding suit of 26 cards, with lower set always having lower value than higher set
+    ## Equally splitting 8 value cards with (spades, hearts) suit to lower set and (diamonds, clubs) suit to higher set
+    for suit in card_suits[:2]:
+        lowerset.append(Card(8, suit))
 
-    for s in card_suits:
-        for t in [(s, q) for q in card_values_lower]:
-            #print("lo", Card(t[1], t[0]))
-            cl.append(Card(t[1], t[0]))
+    for suit in card_suits[2:]:
+        higherset.append(Card(8, suit))
+
+    ## Populating lower and higher decks with list of card objects from 2-7 and 7-14 values respectively
+    for suit in card_suits:
+        for obj in [(suit, value) for value in card_values_lower]:
+            lowerset.append(Card(obj[1], obj[0]))
         
-        for t in [(s, q) for q in card_values_higher]:
-            #print("hi", Card(t[1], t[0]))
-            ch.append(Card(t[1], t[0]))
+        for obj in [(suit, value) for value in card_values_higher]:
+            higherset.append(Card(obj[1], obj[0]))
+
+    # Printing lower card deck
+    print("\nLower set deck: ")
+    for card in lowerset:
+        print("(", card, ")", end=" ")
     
-    for i in card_suits[:2]:
-        cl.append(Card(8, i))
+    # Printing higher card deck
+    print("\nHigher set deck: ")
+    for card in higherset:
+        print("(", card, ")", end=" ")
 
-    for j in card_suits[2:]:
-        ch.append(Card(8, j))
+    # Single round of play to test war
+    set1, set2, winner = play(lowerset, higherset)
+    print("\nWinner after first round of play, Player", winner)
 
-    # for i in cl:
-    #     print("lo", i)
-
-    # for i in ch:
-    #     print("hi", i)
-
-
-    set1, set2, winner = play(cl, ch)
-
+    # Deck exhaustion and game winner logic test
     while set1 and set2:
         set1, set2, winner =  play(set1, set2)
-    assert(winner == 2)
+    print("Winner of the game, Player", winner)
+
+    try:
+        assert(winner==2)
+    except:
+        print("Testcase failed")
+
+# Test 2: Code functionality and logic test
+## Hardcoding deck1 with higher value cards and deck2 with lower value cards and testing play functionality
+def testPlayer1Winner():
+    lowerset, higherset = list(), list()
+
+    # Hardcoding suit of 26 cards, with lower set always having lower value than higher set
+    ## Equally splitting 8 value cards with (spades, hearts) suit to lower set and (diamonds, clubs) suit to higher set
+    for suit in card_suits[:2]:
+        higherset.append(Card(8, suit))
+
+    for suit in card_suits[2:]:
+        lowerset.append(Card(8, suit))
+
+    ## Populating lower and higher decks with list of card objects from 2-7 and 7-14 values respectively
+    for suit in card_suits:
+        for obj in [(suit, value) for value in card_values_lower]:
+            higherset.append(Card(obj[1], obj[0]))
+        
+        for obj in [(suit, value) for value in card_values_higher]:
+            lowerset.append(Card(obj[1], obj[0]))
+
+    # Printing lower card deck
+    print("\nLower set deck: ")
+    for card in lowerset:
+        print("(", card, ")", end=" ")
     
+    # Printing higher card deck
+    print("\nHigher set deck: ")
+    for card in higherset:
+        print("(", card, ")", end=" ")
 
-def test2():
-    cl, ch = list(), list()
-    for s in card_suits[:2]:
-        for t in [(s, q) for q in card_values_lower]:
-            cl.append(Card(t[1], t[0]))
+    # Single round of play to test war
+    set1, set2, winner = play(lowerset, higherset)
+    print("\nWinner after first round of play, Player", winner)
 
-        for t in [(s, q) for q in card_values_higher]:
-            cl.append(Card(t[1], t[0]))
+    # Deck exhaustion and game winner logic test
+    while set1 and set2:
+        set1, set2, winner =  play(set1, set2)
+    print("Winner of the game, Player", winner)
 
+    try:
+        assert(winner==1)
+    except:
+        print("Testcase failed")
+
+# Test 3: Edge case logic test
+## Hardcoding deck1 and deck2 with the exact same set of cards, same values in same order
+def testAlwaysWar():
+    player1set, player2set = list(), list()
+
+    # Hardcoding suit of 26 cards, with both player having the same value decks
+    ## Equally splitting 8 value cards with (spades, hearts) suit to player1 and (diamonds, clubs) suit to player2
+    for suit in card_suits[:2]:
+        player1set.append(Card(8, suit))
+
+    for suit in card_suits[2:]:
+        player2set.append(Card(8, suit))
+
+    ## Populating player1set with all values from 2-7 and 7-14 for (spades, hearts) suit
+    for suit in card_suits[:2]:
+        for obj in [(suit, value) for value in card_values_lower]:
+            player1set.append(Card(obj[1], obj[0]))
+
+        for obj in [(suit, value) for value in card_values_higher]:
+            player1set.append(Card(obj[1], obj[0]))
+
+    ## Populating player2set with all values from 2-7 and 7-14 for (diamonds, clubs) suit
     for s in card_suits[2:]:
-        for t in [(s, q) for q in card_values_lower]:
-            ch.append(Card(t[1], t[0]))
+        for obj in [(suit, value) for value in card_values_lower]:
+            player2set.append(Card(obj[1], obj[0]))
 
-        for t in [(s, q) for q in card_values_higher]:
-            ch.append(Card(t[1], t[0]))
+        for obj in [(suit, value) for value in card_values_higher]:
+            player2set.append(Card(obj[1], obj[0]))
 
-    for i in card_suits[:2]:
-        ch.append(Card(8, i))
+    # Printing player1 card deck
+    print("\nLower set deck: ")
+    for card in player1set:
+        print("(", card, ")", end=" ")
+    
+    # Printing player2 card deck
+    print("\nHigher set deck: ")
+    for card in player2set:
+        print("(", card, ")", end=" ")
 
-    for i in card_suits[2:]:
-        cl.append(Card(8, i))
-    for i in cl:
-        print("lo", i)
+    # Single round of play to test war
+    set1, set2, winner = play(player1set, player2set)
+    if winner == -1:
+        print("\nDrawn Game, both players ran out of their decks simultaneously")
+    else: 
+        print("\nWinner after first round of play, Player", winner)
 
-    for i in ch:
-        print("hi", i)
-    set1, set2, winner = play(cl, ch)
+    # Deck exhaustion and game winner logic test
+    ## The game must terminate on a single round of play, as both players have the same values in same order
     while set1 and set2:
+        print("Execution should never enter this loop")
         set1, set2, winner =  play(set1, set2)
     
-    print(winner)
-print("Test1")
-test1()
-print("Test2")
-test2()
+    try:
+        assert(winner==-1)
+    except:
+        print("Testcase failed")
 
+# Test 4: REST endpoints functionality test
+## Testing initial and updated states of the database by running the /playerwins and /startgame endpoints
+def testEndpoints():
+    # Initial database state
+    ## If the initial win counts is not zero, the testcase fails
+    print("\nTesting playerwins endpoint")
+    initialGameFlag = False
+    initialstate = requests.get(BASE + "/playerwins")
+    print("Initial player win counts per db", initialstate.json())
 
-def test3():
-    response = requests.get(BASE + "startgame")
-    print(response.json())
+    for playerWin in initialstate.json().values():
+        if playerWin == 0:
+            initialGameFlag = True
+        else:
+            initialGameFlag = False
+
+    try:
+        assert initialGameFlag
+    except:
+        print("Testcase failed")
+
+    # Validating database state after 5 random game instances
+    ## If the count of game wins does not match the count stored on the database, the testcase fails
+    print("\nTesting startgame and playerwins endpoint concurrently")
+    fiveGameFlag = False
+    player1wins, player2wins = 0, 0
+    for count in range(1,6):
+        result = requests.get(BASE + "/startgame")
+        print("Winner of round", count, result.json())
+        for player in result.json().values():
+            if player == 'Player 1':
+                player1wins += 1
+            elif player == "Player 2":
+                player2wins += 1
+    
+    gamestate = requests.get(BASE + "/playerwins")
+    print("Updated player win counts per db", gamestate.json())
+    gamewins = []
+    for playerWin in gamestate.json().values():
+        gamewins.append(playerWin)
+    # print(player1wins, player2wins, gamewins)
+    if player1wins != gamewins[0] or player2wins != gamewins[1]:
+        fiveGameFlag = False
+    else:
+        fiveGameFlag = True
+    
+    try:
+        assert fiveGameFlag
+    except:
+        print("Testcase failed")
+
+if __name__ == "__main__":
+    # Test 1
+    print("Test Case 1: Code functionality and logic test")
+    testPlayer2Winner()
+
+    # Test 2
+    print("\nTest Case 2: Code functionality and logic test")
+    testPlayer1Winner()
+
+    # Test 3
+    print("\nTest Case 3: Edge case logic test")
+    testAlwaysWar()
+    
+    # Test 4
+    print("\nTest Case 4: REST endpoints functionality test")
+    testEndpoints()
--- warREST.py
+++ warREST.py
@@ -13,7 +13,8 @@ card_suits = ["spades", "hearts", "diamonds", 'clubs']
 face_values_forward = {14:"A", 13:"K", 12:"Q", 11:"J"}
 face_values_reverse = {"A":14, "K":13, "Q":12, "J":11}
 
-# Card, consisting of a value and suit
+# Card
+## Each card consists of a value and corresponding suit
 class Card:
     def __init__(self, value: int, suit: str):
         self.value = value
@@ -23,6 +24,7 @@ class Card:
         return "{0}, {1}".format(self.value, self.suit)
 
 # Deck of cards
+## Generating 52 cards in a deck, as a list of card objects
 def deck():
     cards = []
     for value in card_values:
@@ -33,7 +35,8 @@ def deck():
                 cards.append(Card(value, suit))
     return cards
 
-# Randomized equal split of playing cards between two players
+# Deck split
+## Randomized equal split of playing cards between two players, each gets 26 cards
 def split_deck(cards):
     set1 = random.sample(cards, 26)
     # Intermediary list for shuffling
@@ -41,84 +44,110 @@ def split_deck(cards):
     # Randomized/shuffled selection from intermediary list
     set2 = random.sample(intset, 26)
     return set1, set2
-i = 0
-# Game play in accordance with defined rules
+
+# Game play
+# A round of game play, each player draws a card and plays round per game rules
 def play(set1, set2):
-    global i
-    i += 1
     winner = -1
-    p1_empty = False
-    p2_empty = False
+    player1_blank = False
+    player2_blank = False
+
     # Drawing cards, play is from first card to last card through pop
     try:
         player1_card = set1.pop(0)
     except:
-       p1_empty = True 
-       #return [], set2, 2
+       player1_blank = True 
     try:
         player2_card = set2.pop(0)
     except:
-       p2_empty = True
-       #return set1, [], 1
+       player2_blank = True
     
-    if p1_empty and p2_empty:
+    # Validating whether players have exhausted their decks
+    if player1_blank and player2_blank:
+        # If both players simultaneously exhaust their decks, no winner
         return [], [], -1
-    elif p1_empty:
+    elif player1_blank:
+        # Player 1 exhausts his deck, Player 2 is the winner
         return [], set2, 2
-    elif p2_empty:
+    elif player2_blank:
+        # Player 2 exhausts his deck, Player 1 is the winner
         return set1, [], 1
 
-    player1_value = player1_card.value if player1_card.value not in face_values_reverse else face_values_reverse[player1_card.value]
-    player2_value = player2_card.value if player2_card.value not in face_values_reverse else face_values_reverse[player2_card.value]
+    # Reverse mapping face cards from representational value to numeric value
+    if player1_card.value in face_values_reverse:
+        player1_value = face_values_reverse[player1_card.value]
+    else:
+        player1_value = player1_card.value
+    if player2_card.value in face_values_reverse:
+        player2_value = face_values_reverse[player2_card.value]
+    else:
+        player2_value = player2_card.value
+    
+    # Game play according to defined game rules
     if player1_value > player2_value:
         winner = 1
-        set1.append(player1_card); set1.append(player2_card)
+        set1.append(player1_card)
+        set1.append(player2_card)
     elif player2_value > player1_value:
         winner = 2
-        set2.append(player2_card); set2.append(player1_card)
+        set2.append(player2_card)
+        set2.append(player1_card)
     elif player1_value == player2_value:
+        # When players are at war, they skip a card in their decks and play another round
+        ## Player 1 exhausts his deck, Player 2 is the winner
         try:
             player1_skip = set1.pop(0)
         except:
             return [] , set2, 2
+        ## Player 2 exhausts his deck, Player 1 is the winner
         try:              
             player2_skip = set2.pop(0)
         except:
             return set1, [], 1
-
-        # Recursive function call
+        ## Players start another round of play, recursive function call
         set1, set2, winner = play(set1, set2)    
+        # Winner of a war gets the skipped card and the cards of his opponent 
         if winner == 1:
-            set1.append(player1_card); set1.append(player2_card)
-            set1.append(player1_skip); set1.append(player2_skip)
+            set1.append(player1_card)
+            set1.append(player2_card)
+            set1.append(player1_skip)
+            set1.append(player2_skip)
         elif winner == 2:
-            set2.append(player2_card); set2.append(player1_card)
-            set2.append(player2_skip); set2.append(player1_skip)
+            set2.append(player2_card)
+            set2.append(player1_card)
+            set2.append(player2_skip)
+            set2.append(player1_skip)
 
     return set1, set2, winner
             
 # Database connection
+## Establishing a connection with sqlite db
 def db_connection():
     conn = sqlite3.connect("playerwins.sqlite")
     return conn
 
+# StartGame Endpoint
+## Inheriting resource building block and overriding HTTP GET method to start war game between two players
 class StartGame(Resource):    
     def get(self):
+        # Connection establishment with db, cursor initialization for query execution
+        conn = db_connection()
+        cursor = conn.cursor()
+
         # List of playing cards
         cards = deck()
 
         # Player's playing card set
         player1_set, player2_set = split_deck(cards)
 
+        # First round of game play
         set1, set2, winner = play(player1_set, player2_set)
-        counter = 0
-        conn = db_connection()
-        cursor = conn.cursor()
 
+        # Gameplay in this game continues till a winner is decided, through exhaustion of decks
         while set1 and set2:
-            counter += 1
             set1, set2, winner = play(set1, set2)
         
+        # Each player's win is persisted in db to keep track of lifetime wins
         if winner == 1:
             cursor = cursor.execute(""" UPDATE playerwins SET wins = wins + 1 WHERE player = "Player1" """)
             conn.commit()
@@ -127,19 +156,24 @@ class StartGame(Resource):
             cursor = cursor.execute(""" UPDATE playerwins SET wins = wins + 1 WHERE player = "Player2" """)
             conn.commit()
             return {"winner:": "Player 2"}
+        elif winner == -1:
+            return {"winner:": "No winner, game has been drawn"}
         else:
             return {"error:": "Error error error"}
 
-
+# Playerwins Endpoint
+## Inheriting resource building block and overriding HTTP GET method to query sqlite db and return player career wins
 class PlayerWins(Resource):
     def get(self):
         conn = db_connection()
         cursor = conn.cursor()
         
+        # Query db and return each player career wins
         cursor = conn.execute(""" SELECT * FROM playerwins """)
         return dict(cursor.fetchall())
 
-
+# Main function
+## Handles flask application creation, endpoint registration
 def main():
     app = Flask(__name__)
     api = Api(app)
